907c6f1df14c75527bdd613e1c0e76704c3550c1
Kyra Taylor <94403670+ktaylor-widen@users.noreply.github.com>
2026-06-24 06:32:18 -0600

    Fix/Support Playwright 1.60+ Locator Class Name (#35)

---
+++ src/utils/__tests__/locator.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test'
+import { isLocator } from '../locator.js'
+
+test.describe('isLocator', () => {
+  test('identifies a Locator', async ({ page }) => {
+    const locator = page.locator('body')
+    expect(isLocator(locator)).toBe(true)
+  })
+
+  test('rejects a Page', async ({ page }) => {
+    expect(isLocator(page)).toBe(false)
+  })
+
+  test('locator constructor name is Locator or _Locator', async ({ page }) => {
+    const locator = page.locator('body')
+    const name = locator.constructor.name
+    expect(['Locator', '_Locator']).toContain(name)
+  })
+})
